fcaa0ae3da3c742c4134e59354c96bf733447abb
David Hazeland <davidhazeland@gmail.com>
2017-03-14 10:57:55 +0700

    Describe

--- generators/saga/templates/test.js
+++ generators/saga/templates/test.js
@@ -5,49 +5,51 @@ import {actions, actionTypes} from '../../index';
 
 import {actions as notificationActions} from 'redux-businesses/lib/notification';
 
-test('[Saga] <%= sagaName %>', () => {
-  const sagaIterator = saga();
-
-  let actual;
-  let expected;
-
-  const parameter = {};
-  const action = actions.<%= action %>(parameter);
-
-  actual = sagaIterator.next().value;
-  expected = take(actionTypes.<%= actionType %>);
-
-  expect(actual).toEqual(expected);
-
-  actual = sagaIterator.next(action).value;
-  expected = fork(handle, action);
-
-  expect(actual).toEqual(expected);
-
-  actual = sagaIterator.next().value;
-  expected = take(actionTypes.<%= actionType %>);
-
-  expect(actual).toEqual(expected);
-});
-
-describe('[Saga] <%= sagaName %> - handle() generator', () => {
-  xtest('Exception', () => {
-    const parameter = {};
-    const action = actions.<%= action %>(parameter);
-    const sagaIterator = handle(action);
+describe('<%= moduleName %>', () => {
+  test('[Saga] <%= sagaName %>', () => {
+    const sagaIterator = saga();
 
     let actual;
     let expected;
 
-    sagaIterator.next();
+    const parameter = {};
+    const action = actions.<%= action %>(parameter);
 
-    const error = new Error('error');
-
-    actual = sagaIterator.throw(error).value;
-    expected = put(notificationActions.notifyFailure({
-      message: 'error'
-    }));
+    actual = sagaIterator.next().value;
+    expected = take(actionTypes.<%= actionType %>);
 
     expect(actual).toEqual(expected);
+
+    actual = sagaIterator.next(action).value;
+    expected = fork(handle, action);
+
+    expect(actual).toEqual(expected);
+
+    actual = sagaIterator.next().value;
+    expected = take(actionTypes.<%= actionType %>);
+
+    expect(actual).toEqual(expected);
+  });
+
+  describe('[Saga] <%= sagaName %> - handle() generator', () => {
+    xtest('Exception', () => {
+      const parameter = {};
+      const action = actions.<%= action %>(parameter);
+      const sagaIterator = handle(action);
+
+      let actual;
+      let expected;
+
+      sagaIterator.next();
+
+      const error = new Error('error');
+
+      actual = sagaIterator.throw(error).value;
+      expected = put(notificationActions.notifyFailure({
+        message: 'error'
+      }));
+
+      expect(actual).toEqual(expected);
+    });
   });
 });
