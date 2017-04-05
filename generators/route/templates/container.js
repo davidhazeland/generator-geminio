31e54074a605d431553153766285c3bf32cd4b46
David Hazeland <davidhazeland@gmail.com>
2017-04-05 11:10:44 +0700

    Add selectors

--- generators/route/templates/container.js
+++ generators/route/templates/container.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 
 import DocumentTitle from 'react-document-title';
 
-import {actions as myActions} from 'businesses/<%= module %>';
+import {actions as myActions, selectors as mySelectors} from 'businesses/<%= module %>';
 import {actions as notifActions, selectors as notifSelectors} from 'ironlake/businesses/notification';
 
 import <%= componentName %>Component from '../components/<%= component %>';
@@ -34,6 +34,7 @@ class <%= containerName %> extends Component {
 
 function mapStateToProps(state) {
   return {
+    ...mySelectors.get(state),
     Notification: notifSelectors.get(state)
   };
 }
